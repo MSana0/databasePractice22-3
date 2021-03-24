ad1a6b6e01233b3bc66e33c599677ed32e44a636
Marium Sana <65021743+MSana0@users.noreply.github.com>
2021-03-24 21:52:42 +0600

    Add files via upload

--- DatabaseFormInsertPractice.php
+++ DatabaseFormInsertPractice.php
@@ -1,25 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Database insert</title>
+	<title>Database Login Insert</title>
 </head>
 <body>
-	<h1>Database Insert</h1>
+	<h1>Database Login Insert</h1>
 	<?php
 
 	$host = "localhost";
-	$username = "mydb_user_1";
+	$username = "mydb1_user_1";
 	$password = "123";
-	$dbname = "mydb";
+	$dbname = "mydb1";
 
 	echo "Mysqli object-oriented";
 	echo "<br>";
+
 	$conn1 = new mysqli($host, $username, $password, $dbname);
-	$res1 = $conn1->connect_errno;
-
-	//can also use "$conn1->connect_errno" in if condition instead of creating another vaariable containing it.
-
-	if($res1){
+	if($conn1->connect_error){
 		echo "1.Database Connection Failed!...";
 		echo "<br>";
 		echo $conn1->connect_error;
@@ -27,22 +24,11 @@
 	else
 	{
 		echo "1.Database Connection Successful!...";
-/*
-		$sql1 = "insert into users (username, password) values ('abc', 123)";
-		if($conn1->query($sql1))
-		{
-			echo "Data Inserted Successfully!";
-		}
-		else 
-		{
-			echo "Failed to insert data.";
-			echo "<br>";
-			echo $conn1->error;
-		}*/
-		$stmt1 = $conn1->prepare("insert into users (username, password) value (?,?)");
+		$sql1 = "insert into users (username, password) values ('abc', '123')";
+		$stmt1 = $conn1->prepare("insert into users (username, password) values (?,?)");
 		$stmt1->bind_param("ss", $p1, $p2);
-		$p1 = "mno";
-		$p2 = "789";
+		$p1 = $_POST['username'];
+		$p2 = $_POST['password'];
 
 		$status = $stmt1->execute();
 		if($status)
@@ -57,50 +43,7 @@
 		}
 	}
 	$conn1->close();
-	echo "<br>";
-	echo "Mysqli Procedural";
-	echo "<br>";
-	//mysqli Procedural
-	$conn2 = mysqli_connect($host, $username, $password, $dbname);
-	if (mysqli_connect_error()) {
-		echo "2.Database Connection Failed!...";
-		echo "<br>";
-		echo mysqli_connect_error();
-	}
-	else{
-		echo "2.Database Connection Successful!...";
-/*
-		$sql2 = "insert into users(username, password) values ('ghi',456)";
-		if(mysqli_query($conn2, $sql2))
-		{
-			echo "Data Insert Successful!";
-		}
-		else
-		{
-			echo "Failed to insert data.";
-			echo "<br>";
-			echo $conn2->error;
-		}
-*/
-		$stmt2 = mysqli_prepare($conn2, "insert into users (username, password) value (?,?)");
-		mysqli_stmt_bind_param(
-		$stmt2, "ss", $p3, $p4);
-		$p3 = "pqr";
-		$p4 = "111";
-		$res = mysqli_stmt_execute($stmt2);
-
-	if($res1){
-		echo "Data Insert Successful!";
-	}
-	else
-	{
-		echo "Failed to insert data.";
-			echo "<br>";
-			echo $conn2->error;
-	}}
-	mysqli_close($conn2);
-
+	
 	?>
-
 </body>
 </html>
